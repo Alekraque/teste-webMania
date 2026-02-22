8b29373949107b0d514e281747c944126767d2f6
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2026-02-22 06:20:12 -0300

    refactor: allow api_token to be nullable in kingdoms table

--- database/migrations/2026_02_20_211926_create_kingdoms_table.php
+++ database/migrations/2026_02_20_211926_create_kingdoms_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('kingdoms', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('api_token')->unique();
+            $table->string('api_token')->nullable();
             $table->timestamps();
         });
     }
